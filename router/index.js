6d07e80a2879fffff678089c044a888a436abacb
陈晓池 <mimeay@zuqiukong.com>
2016-09-29 17:15:27 +0800

    test weixin api

---
+++ router/index.js
@@ -0,0 +1,11 @@
+
+export default {
+  index : ()=>
+    this.body = {
+      code : 0,
+      data : {
+        name : 'chenxiaochi',
+        nickname : 'bendan'
+      }
+    }
+}
--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 // import "babel-polyfill";
+import path from 'path';
 import koa from 'koa';
 import Router from 'koa-router';
 import json from 'koa-json';
 import bodyparser from 'koa-body';
+import views from 'koa-views';
 import indexRouter from './router/index';
+import wxRouter from './router/wx';
 let app = koa();
 const router = Router();
+let v = path.resolve(__dirname,'../views')
+app.use(views('views', {
+  root:v,
+  default: 'ejs'
+}));
 app.use(bodyparser());
 app.use(json());
 
@@ -14,6 +22,7 @@ app
   .use(router.allowedMethods());
 
 router.use('/index',indexRouter.routes(), indexRouter.allowedMethods());
+router.use('/wx',wxRouter.routes(),wxRouter.allowedMethods());
 
 const server = app.listen(3000,()=>{
   console.log("listent:3000");
